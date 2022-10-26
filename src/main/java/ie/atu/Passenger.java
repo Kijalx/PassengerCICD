6e56d37c1fc7e0cce0b3f69d07940429187fd452
Aleks Kijewski <G00396018@atu.ie>
2022-10-26 14:47:54 +0100

    Added Comments to program

--- src/main/java/ie/atu/Passenger.java
+++ src/main/java/ie/atu/Passenger.java
@@ -1,12 +1,19 @@
+/*
+ * Aleksander Kijewski
+ * G00396018
+ * Java Personal Assignment // Test Driven File
+ */
 package ie.atu;
 
 public class Passenger {
-    String userTitle;
-    String userName;
-    String userID;
-    String userPhoneNum;
-    int userAge;
+    /* Creates local variables*/
+    private String userTitle;
+    private String userName;
+    private String userID;
+    private String userPhoneNum;
+    private int userAge;
 
+    /* Constructor */
     public Passenger(String title, String name, String id, String phoneNum, int age){
         setTitle(title);
         setName(name);
@@ -15,66 +22,73 @@ public class Passenger {
         setAge(age);
     }
 
-    public void setTitle(String title){
-        if(title.equalsIgnoreCase("Mr") || title.equalsIgnoreCase("Mrs") || title.equalsIgnoreCase("Ms"))
-        {
-            userTitle = title;
+    /* Setter for UserTitle */
+    private void setTitle(String title){
+        if(title.equalsIgnoreCase("Mr") || title.equalsIgnoreCase("Mrs") || title.equalsIgnoreCase("Ms")){ // Checks if imputted title is correct
+            userTitle = title; // Sets UserTitle
         }
-        else
-        {
-            throw new IllegalArgumentException("This is not a valid title");
+        else { // If title is incorrect
+            throw new IllegalArgumentException("This is not a valid title"); // Outputs an error
         }
     }
-    public String getPassengerTitle(){
+    /* Getter for UserTitle */
+    public String getPassengerTitle() {
         return userTitle;
     }
 
-    public void setName(String nameofuser){
-        if(nameofuser.length() >= 3){
-            userName = nameofuser;
+    /* Setter for UserName */
+    private void setName(String nameofuser) {
+        if(nameofuser.length() >= 3) { // Checks if inputted name is the correct length
+            userName = nameofuser; // Sets userName
         }
-        else{
-            throw new IllegalArgumentException("This is not a valid name must be 3 or more characters");
+        else { // If length is incorrect
+            throw new IllegalArgumentException("This is not a valid name must be 3 or more characters"); // Outputs an error
         }
     }
-    public String getPassengerName(){
+    /* Getter for UserName */
+    public String getPassengerName() {
         return userName;
     }
 
-    public void setUserID(String idofuser){
-        if(idofuser.length() >= 10){
-            userID = idofuser;
+    /* Setter for UserID */
+    private void setUserID(String idofuser) {
+        if(idofuser.length() >= 10) { // Checks length of User id
+            userID = idofuser; // Sets userID
         }
-        else {
-            throw new IllegalArgumentException("ID must be at least 10 characters");
+        else { // If length of userid is incorrect
+            throw new IllegalArgumentException("ID must be at least 10 characters"); // Outputs an error
         }
     }
-    public String getPassengerUserID(){
+    /* Getter for UserID */
+    public String getPassengerUserID() {
         return userID;
     }
 
-    public void setPhoneNumber(String phnum)
-    {
-        if(phnum.length() >= 7){
-            userPhoneNum = phnum;
+    /* Setter for UserPhoneNum */
+    private void setPhoneNumber(String phnum) {
+        if(phnum.length() >= 7) { // Checks length of Phone Number
+            userPhoneNum = phnum; // Sets userPhoneNum
         }
-        else{
-            throw new IllegalArgumentException("Phone Number must be at least 7 characters");
+        else { // If length of phone number is incorrect
+            throw new IllegalArgumentException("Phone Number must be at least 7 characters"); // Outputs an error
         }
     }
-    public String getPassengerPhoneNumber(){
+    /* Getter for UserPhoneNum */
+    public String getPassengerPhoneNumber() {
         return userPhoneNum;
     }
 
-    public void setAge(int ageofuser){
-        if(ageofuser > 16){
-            userAge = ageofuser;
+    /* Setter for UserAge */
+    private void setAge(int ageofuser) {
+        if(ageofuser > 16) { // Checks if users age is above a certain value
+            userAge = ageofuser; // Sets userAge
         }
-        else{
-            throw new IllegalArgumentException("Passenger must be over 16 to fly.");
+        else { // If age is below
+            throw new IllegalArgumentException("Passenger must be over 16 to fly."); // Outputs an error
         }
     }
-    public int getPassengerAge(){
+    /* Getter for UserAge */
+    public int getPassengerAge() {
         return userAge;
     }
 }
--- src/test/java/ie/atu/PassengerTest.java
+++ src/test/java/ie/atu/PassengerTest.java
@@ -1,3 +1,8 @@
+/*
+ * Aleksander Kijewski
+ * G00396018
+ * Java Personal Assignment // Test File
+ */
 package ie.atu;
 
 import org.junit.jupiter.api.AfterEach;
@@ -13,57 +18,57 @@ class PassengerTest {
     }
 
     @Test
-    void testConstructor(){
-        Passenger newPassenger = new Passenger("Mr","Aleks","123JHGE25D2","0834552813",21);
-        assertEquals("Mr", newPassenger.getPassengerTitle());
-        assertEquals("Aleks", newPassenger.getPassengerName());
-        assertEquals("123JHGE25D2", newPassenger.getPassengerUserID());
-        assertEquals("0834552813", newPassenger.getPassengerPhoneNumber());
-        assertEquals(21, newPassenger.getPassengerAge());
+    void testConstructor(){ // Tests the constructor if everything is correct
+        Passenger newPassenger = new Passenger("Mr","Aleks","123JHGE25D2","0834552813",21); // Creates a passenger object
+        assertEquals("Mr", newPassenger.getPassengerTitle()); // Checks User's Title if Mr
+        assertEquals("Aleks", newPassenger.getPassengerName()); // Checks User's Name
+        assertEquals("123JHGE25D2", newPassenger.getPassengerUserID()); // Checks User's ID
+        assertEquals("0834552813", newPassenger.getPassengerPhoneNumber()); // Checks User's Phone Number
+        assertEquals(21, newPassenger.getPassengerAge()); // Checks User's Age
     }
 
     @Test
-    void testConstructorMrs(){
+    void testConstructorMrs(){ // Tests the constructor with Mrs as title
         Passenger newPassenger = new Passenger("Mrs","Aleks","123JHGE25D2","0834552813",21);
-        assertEquals("Mrs", newPassenger.getPassengerTitle());
+        assertEquals("Mrs", newPassenger.getPassengerTitle()); // Checks User's Title if Mrs
     }
 
     @Test
-    void testConstructorMs(){
+    void testConstructorMs(){ // Tests the constructor with Ms as title
         Passenger newPassenger = new Passenger("Ms","Aleks","123JHGE25D2","0834552813",21);
-        assertEquals("Ms", newPassenger.getPassengerTitle());
+        assertEquals("Ms", newPassenger.getPassengerTitle()); // Checks User's Title if Mr
     }
 
     @Test
-    void testTitleFail(){
+    void testTitleFail(){ // Tests when the title is incorect
         Exception exMessage = assertThrows(IllegalArgumentException.class, ()-> new Passenger("sdgs","Aleks","123JHGE25D2","0834552813",21));
-        assertEquals("This is not a valid title", exMessage.getMessage());
+        assertEquals("This is not a valid title", exMessage.getMessage()); // Collects exception error
     }
 
     @Test
-    void testNameFail(){
+    void testNameFail(){ // Tests when the name is incorect length
         Exception exMessage = assertThrows(IllegalArgumentException.class, ()-> new Passenger("Mr","fa","123JHGE25D2","0834552813",21));
         assertEquals("This is not a valid name must be 3 or more characters", exMessage.getMessage());
     }
 
     @Test
-    void idFail(){
+    void idFail(){ // Tests when the ID is incorect length
         Exception exMessage = assertThrows(IllegalArgumentException.class, ()-> new Passenger("Mr","Aleks","1234","0834552813",21));
         assertEquals("ID must be at least 10 characters", exMessage.getMessage());
     }
 
     @Test
-    void phoneNumberFail() {
+    void phoneNumberFail() { // Tests when the phone number is incorect length
         Exception exMessage = assertThrows(IllegalArgumentException.class, ()-> new Passenger("Mr","Aleks","123JHGE25D2","083455",21));
         assertEquals("Phone Number must be at least 7 characters", exMessage.getMessage());
     }
 
     @Test
-    void ageFail(){
+    void ageFail(){ // Tests when the age is too low
         Exception exMessage = assertThrows(IllegalArgumentException.class, ()-> new Passenger("Mr","Aleks","123JHGE25D2","0834552813",12));
         assertEquals("Passenger must be over 16 to fly.", exMessage.getMessage());
     }
-    
+
     @AfterEach
     void tearDown() {
     }
